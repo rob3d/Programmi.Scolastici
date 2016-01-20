286007ccd37cc1d0210c16b694bdc4e8d2015301
santoroberto00 <santoroberto00@gmail.com>
2016-01-20 09:23:23 +0100

    Programma che dati due numeri ne scambia il valore
    semplice programma che scambia due valori interi utilizzando una
    variabile temp come scambio.
    --Aggiunta anche funzione while con messaggio di attesa--

---
+++ scambionumerico.cpp
@@ -0,0 +1,26 @@
+//Autore: Roberto Santo
+
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+	int val1,val2,temp,count;
+	printf("inserisci il primo numero intero:");
+	scanf("%i",&val1);
+	printf("inserisci il secondo numero intero:");
+	scanf("%i",&val2);
+	temp=val1;
+	val1=val2;
+	val2=temp;
+		count=0;
+		while(count<10){
+			printf(".......Processando calcoli quantistici.......\n");
+			count=count+1;
+		}
+	printf("Ecco il nuovo valore dei val1: ");
+	printf("%i\n",val1);
+	printf("Ecco il nuovo valore dei val2: ");
+	printf("%i\n",val2);
+	 system("PAUSE");
+}
+
