e981171bc1b6f2037e98cd3565d96da74b24d019
santoroberto00 <santoroberto00@gmail.com>
2016-01-20 09:55:41 +0100

    pariodispari
    indentazione sistemata

--- pariodispari.cpp
+++ pariodispari.cpp
@@ -8,12 +8,13 @@ main(){
 	printf("Inserisci la variabile: ");
 	scanf("%i",&var);
 	if(var%2==0)
-		{
+	{
 		printf("La variabile inserita e' pari \n");
-		}
-		else{
-			printf("La variabile inserita e' dispari \n'");
-			}
+	}
+	else
+	{
+		printf("La variabile inserita e' dispari \n'");
+	}
 	 system("PAUSE");
 }
 
