24bf93b96213cd5ddb4584d2d9328d6dd0b2466c
santoroberto00 <santoroberto00@gmail.com>
2016-01-20 08:52:33 +0100

    Esercizio per calcolare la media--errato--
    semplice esercizio che calcola la media --in materia errata--

---
+++ media.cpp
@@ -0,0 +1,18 @@
+//Autore: Roberto Santo
+
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+	int a,b;
+	float media;
+	printf("inserisci il primo numero intero:");
+	scanf("%i",&a);
+	printf("inserisci il secondo numero intero:");
+	scanf("%i",&b);
+	printf("la media dei due numeri e':");
+	media=(a+b)/2;
+	printf("%f\n",media);
+	 system("PAUSE");
+}
+
