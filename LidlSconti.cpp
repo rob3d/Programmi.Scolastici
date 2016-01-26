06344371b4b0a34bd68e2068de94e2491343072e
santoroberto00 <santoroberto00@gmail.com>
2016-01-26 22:39:14 +0100

    LidlSconti
    un programma che applica uno sconto se la spesa è maggiore di 100€

---
+++ LidlSconti.cpp
@@ -0,0 +1,35 @@
+//Autore: Roberto Santo
+//LidlSconti
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+	int count;
+	float prezzo , sconto , ps ;
+	printf("Inserisci il prezzo totale della tua spesa:");
+	scanf("%f",&prezzo);
+	
+		count=0;
+		while(count<100)
+		{
+			printf(".......Processando calcoli.......\n");
+			printf("%i percento effettuato.",count);
+			count=count+1;
+			system("cls");
+		}
+		
+	if(prezzo>100)
+	{
+		sconto=prezzo*20/100;
+		ps=prezzo-sconto;
+		printf("Ecco il totale scontato: ");
+		printf("%f\n",ps);
+	}
+	else
+	{
+		printf("Pagah! \n");	
+	}
+	
+	 system("PAUSE");
+}
+
