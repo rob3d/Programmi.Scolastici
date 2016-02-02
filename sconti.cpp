8482406c21fc4dee25eaabdb4b999fe0fabfe8a1
santoroberto00 <santoroberto00@gmail.com>
2016-02-02 19:07:44 +0100

    sconti
    programma che calcola gli sconti in base al prezzo della spesa con la
    funzione if
    PS, da sistemare l indentazione

---
+++ sconti.cpp
@@ -0,0 +1,49 @@
+//Autore: Roberto Santo
+//LidlSconti
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+	float prezzo  ;
+	printf("Inserisci il prezzo totale della tua spesa:");
+	scanf("%f",&prezzo);
+	
+
+		
+	if(prezzo<50)
+	{
+	
+		printf("Lo sconto da applicare e':5%\n ");
+	
+	}
+	else
+	{
+		if(prezzo<65)
+		{
+			printf("Lo sconto da applicare e':6% \n");
+		}
+	}
+	
+		if(prezzo<80&& prezzo>65)
+		{
+			printf("Lo sconto da applicare e':7%\n ");
+		}
+	
+		else
+	{
+		if(prezzo>100)
+		{
+			printf("Lo sconto da applicare e':10%\n ");
+		}
+		else
+		{
+			if(prezzo>80&&prezzo<=100)
+			{
+			printf("Easter egg: dev stanco :D\n");
+			}
+		}
+	}
+	
+	 system("PAUSE");
+}
+
